0a0faf22f83c9daa49b4e9bd438472aed8367265
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2022-09-28 01:12:48 -0500

    Add test.sh script for core

---
+++ core/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source ../.env
+forge test -vv --gas-report --fork-url $FOUNDRY_ETH_RPC_URL --fork-block-number 15348451
+forge test -vv --fork-url $FOUNDRY_ETH_RPC_URL --fork-block-number 15348451 --match-contract MarginAccount
