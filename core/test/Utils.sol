72cf165a208b06fa82057daf480fa6e9dccac804
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2022-11-16 15:29:28 -0600

    Configure Lender.sol to be clonable with immutable args

---
+++ core/test/Utils.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import {ClonesWithImmutableArgs} from "clones-with-immutable-args/ClonesWithImmutableArgs.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
+
+import {InterestModel} from "src/InterestModel.sol";
+import {Lender} from "src/Lender.sol";
+
+function deploySingleLender(ERC20 asset, address treasury, InterestModel interestModel) returns (Lender) {
+    address impl = address(new Lender(treasury, interestModel));
+    address proxy = ClonesWithImmutableArgs.clone(address(impl), abi.encodePacked(address(asset)));
+    return Lender(proxy);
+}
