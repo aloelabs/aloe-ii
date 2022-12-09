cafa8fc2af527affaa7b042920c1f6461057242d
IanWoodard <ianwoodard2000@gmail.com>
2022-12-09 17:46:48 -0500

    Adding support for multiple calls, fixing bugs, and adding tests

--- periphery/src/BorrowManager.sol
+++ periphery/src/BorrowManager.sol
@@ -21,39 +21,46 @@ contract BorrowManager is IManager {
 
     /**
      * @notice This should be called by a borrower during its modify function
-     * @param data intent, amount0, amount1
-     * @dev intent: 0 = borrow, 1 = repay, 2 = withdraw
+     * @param data actions, amount0, amount1
+     * @dev actions: 0 = borrow, 1 = repay, 2 = withdraw
      * @return positions
      * @return includeLenderReceipts
      */
-    function callback(
-        bytes calldata data
-    ) public returns (Uniswap.Position[] memory positions, bool includeLenderReceipts) {
+    function callback(bytes calldata data)
+        public
+        returns (Uniswap.Position[] memory positions, bool includeLenderReceipts)
+    {
         require(FACTORY.isBorrower(msg.sender), "Aloe: bad account");
         Borrower account = Borrower(msg.sender);
-        (uint8 intent, uint256 amount0, uint256 amount1) = abi.decode(data, (uint8, uint256, uint256));
-        if (intent == 0) {
-            account.borrow(amount0, amount1, account.owner());
-        } else if (intent == 1) {
-            if (amount0 != 0) {
-                ERC20 token0 = account.TOKEN0();
-                Lender lender0 = account.LENDER0();
-                token0.safeTransferFrom(account.owner(), address(lender0), amount0);
-                lender0.repay(amount0, address(this));
-            }
-            if (amount1 != 0) {
-                ERC20 token1 = account.TOKEN1();
-                Lender lender1 = account.LENDER1();
-                token1.safeTransferFrom(account.owner(), address(lender1), amount1);
-                lender1.repay(amount1, address(this));
-            }
-        } else if (intent == 2) {
-            if (amount0 != 0) {
-                ERC20 token0 = account.TOKEN0();
-                token0.safeTransferFrom(address(account), account.owner(), amount0);
-            } else {
-                ERC20 token1 = account.TOKEN1();
-                token1.safeTransferFrom(address(account), account.owner(), amount1);
+        (uint8[] memory actions, bytes[] memory args) = abi.decode(data, (uint8[], bytes[]));
+        require(actions.length == args.length, "Aloe: bad data");
+        for (uint256 i; i < actions.length; i++) {
+            uint8 action = actions[i];
+            (uint256 amount0, uint256 amount1) = abi.decode(args[i], (uint256, uint256));
+            if (action == 0) {
+                account.borrow(amount0, amount1, account.owner());
+            } else if (action == 1) {
+                if (amount0 != 0) {
+                    ERC20 token0 = account.TOKEN0();
+                    Lender lender0 = account.LENDER0();
+                    token0.safeTransferFrom(account.owner(), address(lender0), amount0);
+                    lender0.repay(amount0, address(account));
+                }
+                if (amount1 != 0) {
+                    ERC20 token1 = account.TOKEN1();
+                    Lender lender1 = account.LENDER1();
+                    token1.safeTransferFrom(account.owner(), address(lender1), amount1);
+                    lender1.repay(amount1, address(account));
+                }
+            } else if (action == 2) {
+                if (amount0 != 0) {
+                    ERC20 token0 = account.TOKEN0();
+                    token0.safeTransferFrom(address(account), account.owner(), amount0);
+                }
+                if (amount1 != 0) {
+                    ERC20 token1 = account.TOKEN1();
+                    token1.safeTransferFrom(address(account), account.owner(), amount1);
+                }
             }
         }
     }
