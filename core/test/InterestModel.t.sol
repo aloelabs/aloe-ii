a682da7629d38a34f7f8174422fc86871b1130b2
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2022-12-25 18:54:05 -0600

    Add more InterestModel tests

--- core/test/InterestModel.t.sol
+++ core/test/InterestModel.t.sol
@@ -19,4 +19,34 @@ contract RateModelTest is Test {
         assertGe(result, 1e12);
         assertLt(result, 2e12);
     }
+
+    function test_monotonic(uint256 utilization) public {
+        vm.assume(utilization != 0);
+
+        assertGe(model.getAccrualFactor(13, utilization), model.getAccrualFactor(13, utilization - 1));
+    }
+
+    function test_matchesSpec() public {
+        assertEq(model.getAccrualFactor(0, 0), 1e12);
+        assertEq(model.getAccrualFactor(13, 0), 1e12);
+        assertEq(model.getAccrualFactor(365 days, 0), 1e12);
+
+        assertEq(model.getAccrualFactor(0, 0.1e18), 1e12);
+        assertEq(model.getAccrualFactor(13, 0.1e18), 1000000000871);
+        assertEq(model.getAccrualFactor(1 days, 0.1e18), 1000005788813);
+        assertEq(model.getAccrualFactor(1 weeks, 0.1e18), 1000040522394);
+        assertEq(model.getAccrualFactor(365 days, 0.1e18), 1000040522394);
+
+        assertEq(model.getAccrualFactor(0, 0.8e18), 1e12);
+        assertEq(model.getAccrualFactor(13, 0.8e18), 1000000031720);
+        assertEq(model.getAccrualFactor(1 days, 0.8e18), 1000210838121);
+        assertEq(model.getAccrualFactor(1 weeks, 0.8e18), 1001476800691);
+        assertEq(model.getAccrualFactor(365 days, 0.8e18), 1001476800691);
+
+        assertEq(model.getAccrualFactor(0, 0.999e18), 1e12);
+        assertEq(model.getAccrualFactor(13, 0.999e18), 1000000785200);
+        assertEq(model.getAccrualFactor(1 days, 0.999e18), 1005232198483);
+        assertEq(model.getAccrualFactor(1 weeks, 0.999e18), 1037205322874);
+        assertEq(model.getAccrualFactor(365 days, 0.999e18), 1037205322874);
+    }
 }
