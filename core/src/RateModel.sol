17adf064e041def2efc58a0b80fa36ab326601e4
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2023-01-11 00:44:43 -0600

    Move conditional, RateModel.getAccrualFactor -> computeYieldPerSecond (#16)

--- core/src/RateModel.sol
+++ core/src/RateModel.sol
@@ -14,13 +14,7 @@ contract RateModel {
 
     function getAccrualFactor(uint256 elapsedTime, uint256 utilization) external pure returns (uint256) {
         unchecked {
-            uint256 rate;
-
-            if (utilization < 0.99e18) {
-                rate = computeYieldPerSecond(utilization);
-            } else {
-                rate = 1000000060400;
-            }
+            uint256 rate = computeYieldPerSecond(utilization);
 
             if (elapsedTime > 1 weeks) elapsedTime = 1 weeks;
 
@@ -30,7 +24,7 @@ contract RateModel {
 
     function computeYieldPerSecond(uint256 utilization) public pure returns (uint256) {
         unchecked {
-            return B + A / (1e18 - utilization);
+            return (utilization < 0.99e18) ? B + A / (1e18 - utilization) : 1000000060400;
         }
     }
 }
