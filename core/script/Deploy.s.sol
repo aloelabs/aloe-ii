df63dbe8795e923ac9719b4f7af1d9ec1f38491f
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2023-01-22 15:49:45 -0600

    Use Pr00xy create2 factory in deploy scripts

--- core/script/Deploy.s.sol
+++ core/script/Deploy.s.sol
@@ -3,19 +3,49 @@ pragma solidity 0.8.17;
 
 import "forge-std/Script.sol";
 
-import {Factory, IUniswapV3Pool} from "../src/Factory.sol";
+import {Factory} from "../src/Factory.sol";
 import {RateModel} from "../src/RateModel.sol";
 
+interface ImmutableCreate2Factory {
+    function safeCreate2(bytes32 salt, bytes memory initCode) external returns (address);
+
+    function findCreate2Address(bytes32 salt, bytes memory initCode) external view returns (address);
+
+    function findCreate2AddressViaHash(bytes32 salt, bytes32 initCodeHash) external view returns (address);
+
+    function hasBeenDeployed(address deploymentAddress) external view returns (bool);
+}
+
+ImmutableCreate2Factory constant PR00XY_FACTORY = ImmutableCreate2Factory(0x0000000000FFe8B47B3e2130213B802212439497);
+
+bytes32 constant SALT_ALOE_II_RATE_MODEL = 0xbbc2cd847bdf10468861dab854cd2b2e315e28c82d7041b11db008801ba5ea43;
+bytes32 constant SALT_ALOE_II_FACTORY = 0xbbc2cd847bdf10468861dab854cd2b2e315e28c84f81a0a48c5d550005aa9b33;
+
 contract DeployScript is Script {
+    event GetInitHash(bytes32 initHash);
+
     function run() external {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
 
-        RateModel rateModel = new RateModel();
-        Factory factory = new Factory(rateModel);
+        bytes memory args;
+        bytes memory creationCode;
+        bytes memory initializationCode;
 
-        // Create some markets to kick things off
-        factory.createMarket(IUniswapV3Pool(0xfBe57C73A82171A773D3328F1b563296151be515));
-        factory.createMarket(IUniswapV3Pool(0xc0A1c271efD6D6325D5db33db5e7cF42A715CD12));
+        creationCode = type(RateModel).creationCode;
+        initializationCode = abi.encodePacked(creationCode);
+        address rateModel = PR00XY_FACTORY.safeCreate2(SALT_ALOE_II_RATE_MODEL, initializationCode);
+
+        assert(rateModel == 0x00000000a6f937BCB46F1dB682Ce4F0CDD99Afb1);
+
+        args = abi.encode(rateModel);
+        creationCode = type(Factory).creationCode;
+        initializationCode = abi.encodePacked(creationCode, args);
+
+        emit GetInitHash(keccak256(initializationCode));
+
+        address factory = PR00XY_FACTORY.safeCreate2(SALT_ALOE_II_FACTORY, initializationCode);
+
+        assert(factory == 0x00001e0800ef386E00005Ad9e11C82c8b800BF4f);
 
         vm.stopBroadcast();
     }
--- periphery/script/Deploy.s.sol
+++ periphery/script/Deploy.s.sol
@@ -6,22 +6,53 @@ import "forge-std/Script.sol";
 import {Factory} from "aloe-ii-core/Factory.sol";
 
 import {BorrowerLens} from "src/BorrowerLens.sol";
-import {BorrowManager} from "src/BorrowManager.sol";
 import {FrontendManager} from "src/FrontendManager.sol";
 import {LenderLens} from "src/LenderLens.sol";
 import {Router} from "src/Router.sol";
 
-Factory constant FACTORY = Factory(address(0));
+interface ImmutableCreate2Factory {
+    function safeCreate2(bytes32 salt, bytes memory initCode) external returns (address);
+
+    function findCreate2Address(bytes32 salt, bytes memory initCode) external view returns (address);
+
+    function findCreate2AddressViaHash(bytes32 salt, bytes32 initCodeHash) external view returns (address);
+
+    function hasBeenDeployed(address deploymentAddress) external view returns (bool);
+}
+
+ImmutableCreate2Factory constant PR00XY_FACTORY = ImmutableCreate2Factory(0x0000000000FFe8B47B3e2130213B802212439497);
+
+address constant ALOE_II_FACTORY = 0x00001e0800ef386E00005Ad9e11C82c8b800BF4f;
+bytes32 constant SALT_ALOE_II_FRONTEND_MANAGER = 0xbbc2cd847bdf10468861dab854cd2b2e315e28c8a03bc66dcbb0c001f6dbf5bd;
+bytes32 constant SALT_ALOE_II_LENDER_LENS = 0xbbc2cd847bdf10468861dab854cd2b2e315e28c8a03bc66dcbb0c001f6dbf5bd;
+bytes32 constant SALT_ALOE_II_BORROWER_LENS = 0xbbc2cd847bdf10468861dab854cd2b2e315e28c8a03bc66dcbb0c001f6dbf5bd;
+bytes32 constant SALT_ALOE_II_ROUTER = 0xbbc2cd847bdf10468861dab854cd2b2e315e28c8a03bc66dcbb0c001f6dbf5bd;
 
 contract DeployScript is Script {
     function run() external {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
 
-        new BorrowerLens();
-        new BorrowManager(FACTORY);
-        new FrontendManager(FACTORY);
-        new LenderLens();
-        new Router();
+        bytes memory args;
+        bytes memory creationCode;
+        bytes memory initializationCode;
+
+        args = abi.encode(ALOE_II_FACTORY);
+        creationCode = type(FrontendManager).creationCode;
+        initializationCode = abi.encodePacked(creationCode, args);
+        address frontendManager = PR00XY_FACTORY.safeCreate2(SALT_ALOE_II_FRONTEND_MANAGER, initializationCode);
+        assert(frontendManager == 0x000071E5490031009cbE0000866968003ebe0081);
+
+        creationCode = type(LenderLens).creationCode;
+        initializationCode = abi.encodePacked(creationCode);
+        PR00XY_FACTORY.safeCreate2(SALT_ALOE_II_LENDER_LENS, initializationCode);
+
+        creationCode = type(BorrowerLens).creationCode;
+        initializationCode = abi.encodePacked(creationCode);
+        PR00XY_FACTORY.safeCreate2(SALT_ALOE_II_BORROWER_LENS, initializationCode);
+
+        creationCode = type(Router).creationCode;
+        initializationCode = abi.encodePacked(creationCode);
+        PR00XY_FACTORY.safeCreate2(SALT_ALOE_II_ROUTER, initializationCode);
 
         vm.stopBroadcast();
     }
