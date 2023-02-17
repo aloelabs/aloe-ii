30175fc1180cc5037a00534c15be62f1cffeb0c0
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2023-02-16 22:25:24 -0600

    Create SimpleManager and WithdrawManager

--- periphery/script/Deploy.s.sol
+++ periphery/script/Deploy.s.sol
@@ -6,9 +6,11 @@ import "forge-std/Script.sol";
 import {Factory} from "aloe-ii-core/Factory.sol";
 
 import {BorrowerLens} from "src/BorrowerLens.sol";
-import {FrontendManager} from "src/FrontendManager.sol";
 import {LenderLens} from "src/LenderLens.sol";
 import {Router} from "src/Router.sol";
+import {FrontendManager} from "src/managers/FrontendManager.sol";
+import {SimpleManager} from "src/managers/SimpleManager.sol";
+import {WithdrawManager} from "src/managers/WithdrawManager.sol";
 
 Factory constant ALOE_II_FACTORY = Factory(0x95110C9806833d3D3C250112fac73c5A6f631E80);
 bytes32 constant TAG = bytes32(uint256(0xA10EBE1A));
@@ -17,10 +19,13 @@ contract DeployScript is Script {
     function run() external {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
 
-        new BorrowerLens{salt: TAG}();
-        new FrontendManager{salt: TAG}(ALOE_II_FACTORY);
-        new LenderLens{salt: TAG}();
-        new Router{salt: TAG}();
+        // new BorrowerLens{salt: TAG}();
+        // new LenderLens{salt: TAG}();
+        // new Router{salt: TAG}();
+
+        // new FrontendManager{salt: TAG}(ALOE_II_FACTORY);
+        new SimpleManager{salt: TAG}();
+        new WithdrawManager{salt: TAG}();
 
         vm.stopBroadcast();
     }
---
+++ periphery/src/managers/SimpleManager.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.17;
+
+import {IManager} from "aloe-ii-core/Borrower.sol";
+
+contract SimpleManager is IManager {
+    function callback(bytes calldata data) external override returns (uint144) {
+        (bool success, ) = msg.sender.call(data);
+        require(success);
+        return 0;
+    }
+}
