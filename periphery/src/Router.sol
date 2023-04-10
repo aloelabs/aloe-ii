8334a26538999a527ff02b4fa7fdaebce1477ca2
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2023-04-09 23:11:10 -0500

    Add Permit2 to Router

--- periphery/src/Router.sol
+++ periphery/src/Router.sol
@@ -8,6 +8,12 @@ import {Lender} from "aloe-ii-core/Lender.sol";
 contract Router {
     using SafeTransferLib for ERC20;
 
+    IPermit2 public immutable PERMIT2;
+
+    constructor(IPermit2 permit2) {
+        PERMIT2 = permit2;
+    }
+
     function depositWithApprove(Lender lender, uint256 amount) external returns (uint256 shares) {
         lender.asset().safeTransferFrom(msg.sender, address(lender), amount);
         shares = lender.deposit(amount, msg.sender);
@@ -70,6 +76,35 @@ contract Router {
         shares = lender.deposit(amount, msg.sender);
     }
 
+    function depositWithPermit2(
+        Lender lender,
+        uint256 amount,
+        uint256 nonce,
+        uint256 deadline,
+        bytes calldata signature
+    ) external returns (uint256 shares) {
+        // Transfer tokens from the caller to the lender.
+        PERMIT2.permitTransferFrom(
+            // The permit message.
+            IPermit2.PermitTransferFrom({
+                permitted: IPermit2.TokenPermissions({token: lender.asset(), amount: amount}),
+                nonce: nonce,
+                deadline: deadline
+            }),
+            // The transfer recipient and amount.
+            IPermit2.SignatureTransferDetails({to: address(lender), requestedAmount: amount}),
+            // The owner of the tokens, which must also be
+            // the signer of the message, otherwise this call
+            // will fail.
+            msg.sender,
+            // The packed signature that was the result of signing
+            // the EIP712 hash of `permit`.
+            signature
+        );
+
+        shares = lender.deposit(amount, msg.sender);
+    }
+
     function repayWithApprove(Lender lender, uint256 amount, address beneficiary) external returns (uint256 units) {
         lender.asset().safeTransferFrom(msg.sender, address(lender), amount);
         units = lender.repay(amount, beneficiary);
@@ -94,4 +129,42 @@ contract Router {
         asset.safeTransferFrom(msg.sender, address(lender), amount);
         units = lender.repay(amount, beneficiary);
     }
+}
+
+// Minimal Permit2 interface, derived from
+// https://github.com/Uniswap/permit2/blob/main/src/interfaces/ISignatureTransfer.sol
+interface IPermit2 {
+    // Token and amount in a permit message.
+    struct TokenPermissions {
+        // Token to transfer.
+        ERC20 token;
+        // Amount to transfer.
+        uint256 amount;
+    }
+
+    // The permit2 message.
+    struct PermitTransferFrom {
+        // Permitted token and amount.
+        TokenPermissions permitted;
+        // Unique identifier for this permit.
+        uint256 nonce;
+        // Expiration for this permit.
+        uint256 deadline;
+    }
+
+    // Transfer details for permitTransferFrom().
+    struct SignatureTransferDetails {
+        // Recipient of tokens.
+        address to;
+        // Amount to transfer.
+        uint256 requestedAmount;
+    }
+
+    // Consume a permit2 message and transfer tokens.
+    function permitTransferFrom(
+        PermitTransferFrom calldata permit,
+        SignatureTransferDetails calldata transferDetails,
+        address owner,
+        bytes calldata signature
+    ) external;
 }
