dd98f1d6ff99da343df6941ba271ca7e35928cf5
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2023-05-09 02:54:51 -0500

    Add ERC4626 invariant tests

---
+++ core/test/invariants/ERC4626Harness.sol
@@ -0,0 +1,306 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.17;
+
+import "forge-std/Vm.sol";
+
+import {ERC4626, ERC20} from "solmate/mixins/ERC4626.sol";
+import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
+
+import "src/Lender.sol";
+
+contract ERC4626Harness {
+    event Deposit(address indexed caller, address indexed owner, uint256 assets, uint256 shares);
+
+    event Withdraw(
+        address indexed caller,
+        address indexed receiver,
+        address indexed owner,
+        uint256 assets,
+        uint256 shares
+    );
+
+    Vm constant vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+
+    ERC4626 immutable VAULT;
+
+    address[] public holders;
+
+    mapping(address => bool) alreadyHolder;
+
+    /*//////////////////////////////////////////////////////////////
+                              CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
+    constructor(Lender lender) {
+        VAULT = ERC4626(address(lender));
+
+        holders.push(lender.RESERVE());
+        alreadyHolder[lender.RESERVE()] = true;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                  MAIN
+    //////////////////////////////////////////////////////////////*/
+
+    /// @dev Don't mess with the logic here, things get weird. See https://github.com/foundry-rs/foundry/issues/3806
+    function warp(uint16 elapsedTime) external {
+        uint256 t0 = block.timestamp;
+        uint256 t1 = t0 + elapsedTime;
+        if (elapsedTime > 0) {
+            vm.warp(t1);
+            require(block.timestamp == t1, "warp failed");
+        }
+    }
+
+    function deposit(uint256 amount, address receiver, bool shouldPrepay) public returns (uint256 shares) {
+        amount = amount % (VAULT.maxDeposit(msg.sender) + 1); // TODO: if remove this, could run with reverts allowed
+
+        ERC20 asset = VAULT.asset();
+        uint256 balance = asset.balanceOf(msg.sender);
+
+        // MUST return as close to and no more than the exact amount of shares that would be minted in a `deposit` call
+        shares = VAULT.previewDeposit(amount);
+
+        // Make sure `msg.sender` has enough assets to deposit
+        if (amount > balance) {
+            vm.prank(msg.sender);
+            vm.expectRevert(bytes(shares > 0 ? "TRANSFER_FROM_FAILED" : "Aloe: zero impact"));
+            VAULT.deposit(amount, receiver);
+
+            MockERC20 mock = MockERC20(address(asset));
+            mock.mint(msg.sender, amount - balance);
+            balance = amount;
+        }
+
+        // Collect data
+        uint256 totalAssets = VAULT.totalAssets();
+        // uint256 totalSupply = VAULT.totalSupply();
+        uint256 sharesBefore = VAULT.balanceOf(receiver);
+
+        // Actual action
+        // --> Pre-pay or approve
+        vm.prank(msg.sender);
+        if (shouldPrepay) asset.transfer(address(VAULT), amount); // MAY support an additional flow
+        else asset.approve(address(VAULT), amount); // MUST support EIP-20 `approve`/`transferFrom` flow
+        // --> Make deposit
+        if (shares == 0) {
+            vm.prank(msg.sender);
+            vm.expectRevert(bytes("Aloe: zero impact"));
+            VAULT.deposit(amount, receiver);
+            amount = 0;
+        } else {
+            vm.prank(msg.sender);
+            vm.expectEmit(true, true, false, true, address(VAULT)); // MUST emit `Deposit` event
+            emit Deposit(msg.sender, receiver, amount, shares);
+            require(VAULT.deposit(amount, receiver) == shares, "deposit: incorrect preview");
+        }
+
+        // Assertions
+        require(asset.balanceOf(msg.sender) == balance - amount, "deposit: payment issue");
+        require(VAULT.totalAssets() == totalAssets + amount, "deposit: totalAssets mismatch");
+        // NOTE: This doesn't hold when interest accrues (because shares are minted to reserves)
+        // require(VAULT.totalSupply() == totalSupply + shares, "deposit: totalSupply mismatch");
+        if (receiver != holders[0]) {
+            require(VAULT.balanceOf(receiver) == sharesBefore + shares, "deposit: mint issue");
+        }
+
+        // {HARNESS BOOKKEEPING} Keep holders up-to-date
+        if (!alreadyHolder[receiver]) {
+            holders.push(receiver);
+            alreadyHolder[receiver] = true;
+        }
+    }
+
+    function mint(uint256 shares, address receiver, bool shouldPrepay) public returns (uint256 amount) {
+        shares = shares % (VAULT.maxMint(msg.sender) + 1); // TODO: if remove this, could run with reverts allowed
+
+        ERC20 asset = VAULT.asset();
+        uint256 balance = asset.balanceOf(msg.sender);
+
+        // MUST return as close to and no more than the exact amount of assets that would be deposited in a `mint` call
+        amount = VAULT.previewMint(shares);
+
+        // Make sure `msg.sender` has enough assets to mint
+        if (amount > balance) {
+            vm.prank(msg.sender);
+            vm.expectRevert(bytes(shares > 0 ? "TRANSFER_FROM_FAILED" : "Aloe: zero impact"));
+            VAULT.mint(amount, receiver);
+
+            MockERC20 mock = MockERC20(address(asset));
+            mock.mint(msg.sender, amount - balance);
+            balance = amount;
+        }
+
+        // Collect data
+        uint256 totalAssets = VAULT.totalAssets();
+        uint256 sharesBefore = VAULT.balanceOf(receiver);
+
+        // Actual action
+        // --> Pre-pay or approve
+        vm.prank(msg.sender);
+        if (shouldPrepay) asset.transfer(address(VAULT), amount); // MAY support an additional flow
+        else asset.approve(address(VAULT), amount); // MUST support EIP-20 `approve`/`transferFrom` flow
+        // --> Make mint
+        if (shares == 0) {
+            vm.prank(msg.sender);
+            vm.expectRevert(bytes("Aloe: zero impact"));
+            VAULT.mint(shares, receiver);
+        } else {
+            vm.prank(msg.sender);
+            vm.expectEmit(true, true, false, true, address(VAULT)); // MUST emit `Deposit` event
+            emit Deposit(msg.sender, receiver, amount, shares);
+            require(VAULT.mint(shares, receiver) == amount, "mint: incorrect preview");
+        }
+
+        // Assertions
+        require(asset.balanceOf(msg.sender) == balance - amount, "mint: payment issue");
+        require(VAULT.totalAssets() == totalAssets + amount, "mint: totalAssets mismatch");
+        if (receiver != holders[0]) {
+            require(VAULT.balanceOf(receiver) == sharesBefore + shares, "mint: mint issue");
+        }
+        // NOTE: We don't make assertions about the change in `totalSupply` because when interest accrues,
+        // shares are minted to reserves (separate from the operations being tested).
+
+        // {HARNESS BOOKKEEPING} Keep holders up-to-date
+        if (!alreadyHolder[receiver]) {
+            holders.push(receiver);
+            alreadyHolder[receiver] = true;
+        }
+    }
+
+    function redeem(uint256 shares, address receiver, address owner) public returns (uint256 assets) {
+        // MUST revert if all of `shares` cannot be redeemed
+        uint256 maxRedeem = VAULT.maxRedeem(owner);
+        if (shares > maxRedeem) {
+            vm.prank(msg.sender);
+            vm.expectRevert();
+            VAULT.redeem(shares, receiver, owner);
+
+            shares = shares % (maxRedeem + 1);
+        }
+
+        // SHOULD check `msg.sender` can spend owner funds using allowance
+        if (owner != msg.sender) {
+            vm.prank(msg.sender);
+            vm.expectRevert();
+            VAULT.redeem(shares, receiver, owner);
+
+            vm.prank(owner);
+            VAULT.approve(msg.sender, shares);
+        }
+
+        // Collect data
+        assets = VAULT.previewRedeem(shares);
+        uint256 totalAssets = VAULT.totalAssets();
+        uint256 sharesBefore = VAULT.balanceOf(owner);
+        uint256 assetsBefore = VAULT.asset().balanceOf(receiver);
+
+        // Actual action
+        if (assets == 0) {
+            vm.prank(msg.sender);
+            vm.expectRevert(bytes("Aloe: zero impact"));
+            VAULT.redeem(shares, receiver, owner);
+            shares = 0;
+        } else {
+            vm.prank(msg.sender);
+            vm.expectEmit(true, true, true, true, address(VAULT)); // MUST emit `Withdraw` event
+            emit Withdraw(msg.sender, receiver, owner, assets, shares);
+            require(VAULT.redeem(shares, receiver, owner) == assets, "redeem: incorrect preview");
+        }
+
+        // Assertions
+        require(VAULT.totalAssets() == totalAssets - assets, "redeem: totalAssets mismatch");
+        if (receiver != address(VAULT)) {
+            require(VAULT.asset().balanceOf(receiver) == assetsBefore + assets, "redeem: transfer issue");
+        } else {
+            require(VAULT.asset().balanceOf(receiver) == assetsBefore, "redeem: bad self reference");
+        }
+        if (receiver != holders[0]) {
+            require(VAULT.balanceOf(owner) == sharesBefore - shares, "redeem: burn issue");
+        }
+        // NOTE: We don't make assertions about the change in `totalSupply` because when interest accrues,
+        // shares are minted to reserves (separate from the operations being tested).
+    }
+
+    function withdraw(uint256 assets, address receiver, address owner) public returns (uint256 shares) {
+        // MUST revert if all of `assets` cannot be withdrawn
+        uint256 maxWithdraw = VAULT.maxWithdraw(owner);
+        if (assets > maxWithdraw) {
+            vm.prank(msg.sender);
+            vm.expectRevert();
+            VAULT.withdraw(assets, receiver, owner);
+
+            assets = assets % (maxWithdraw + 1);
+        }
+
+        // SHOULD check `msg.sender` can spend owner funds using allowance
+        shares = VAULT.previewWithdraw(assets);
+        if (owner != msg.sender) {
+            vm.prank(msg.sender);
+            vm.expectRevert();
+            VAULT.withdraw(assets, receiver, owner);
+
+            vm.prank(owner);
+            VAULT.approve(msg.sender, shares);
+        }
+
+        // Collect data
+        uint256 totalAssets = VAULT.totalAssets();
+        uint256 sharesBefore = VAULT.balanceOf(owner);
+        uint256 assetsBefore = VAULT.asset().balanceOf(receiver);
+
+        // Actual action
+        if (assets == 0) {
+            vm.prank(msg.sender);
+            vm.expectRevert(bytes("Aloe: zero impact"));
+            VAULT.withdraw(assets, receiver, owner);
+            shares = 0;
+        } else {
+            vm.prank(msg.sender);
+            vm.expectEmit(true, true, true, true, address(VAULT)); // MUST emit `Withdraw` event
+            emit Withdraw(msg.sender, receiver, owner, assets, shares);
+            require(VAULT.withdraw(shares, receiver, owner) == shares, "withdraw: incorrect preview");
+        }
+
+        // Assertions
+        require(VAULT.totalAssets() == totalAssets - assets, "withdraw: totalAssets mismatch");
+        if (receiver != address(VAULT)) {
+            require(VAULT.asset().balanceOf(receiver) == assetsBefore + assets, "withdraw: transfer issue");
+        } else {
+            require(VAULT.asset().balanceOf(receiver) == assetsBefore, "withdraw: bad self reference");
+        }
+        if (receiver != holders[0]) {
+            require(VAULT.balanceOf(owner) == sharesBefore - shares, "withdraw: burn issue");
+        }
+        // NOTE: We don't make assertions about the change in `totalSupply` because when interest accrues,
+        // shares are minted to reserves (separate from the operations being tested).
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            HELP THE FUZZER
+    //////////////////////////////////////////////////////////////*/
+
+    function redeemStandard(uint256 shares, address receiver) external returns (uint256 assets) {
+        assets = redeem(shares, receiver, msg.sender);
+    }
+
+    function redeemMax(address receiver) external returns (uint256 assets) {
+        assets = redeem(VAULT.maxRedeem(msg.sender), receiver, msg.sender);
+    }
+
+    function withdrawStandard(uint256 assets, address receiver) external returns (uint256 shares) {
+        shares = withdraw(assets, receiver, msg.sender);
+    }
+
+    function withdrawMax(address receiver) external returns (uint256 shares) {
+        shares = withdraw(VAULT.maxWithdraw(msg.sender), receiver, msg.sender);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                             ARRAY LENGTHS
+    //////////////////////////////////////////////////////////////*/
+
+    function getHolderCount() external view returns (uint256) {
+        return holders.length;
+    }
+}
---
+++ core/test/invariants/ERC4626Invariants.t.sol
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.17;
+
+import "forge-std/Test.sol";
+import "forge-std/Vm.sol";
+
+import {ClonesWithImmutableArgs} from "clones-with-immutable-args/ClonesWithImmutableArgs.sol";
+import {ERC4626, ERC20} from "solmate/mixins/ERC4626.sol";
+import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
+
+import "src/Lender.sol";
+
+import {ERC4626Harness} from "./ERC4626Harness.sol";
+
+contract ERC4626InvariantsTest is Test {
+    ERC20 public asset;
+
+    ERC4626 public vault;
+
+    ERC4626Harness public vaultHarness;
+
+    struct ThingsThatShouldntChange {
+        string name;
+        string symbol;
+        uint8 decimals;
+        ERC20 asset;
+        bytes32 domainSeparator;
+    }
+
+    ThingsThatShouldntChange public thingsThatShouldntChange;
+
+    function setUp() public {
+        {
+            asset = new MockERC20("Token", "TKN", 18); // TODO: replace 18 with an env var
+            address lenderImplementation = address(new Lender(address(2)));
+            Lender lender = Lender(ClonesWithImmutableArgs.clone(
+                lenderImplementation,
+                abi.encodePacked(address(asset))
+            ));
+            RateModel rateModel = new RateModel();
+            lender.initialize(rateModel, 8); // TODO: replace 8 with an env var
+
+            vault = ERC4626(address(lender));
+            vaultHarness = new ERC4626Harness(lender);
+
+            targetContract(address(vaultHarness));
+
+            // forge can't simulate transactions from addresses with code, so we must exclude all contracts
+            excludeSender(address(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D)); // vm
+            excludeSender(address(0x4e59b44847b379578588920cA78FbF26c0B4956C)); // built-in create2 deployer
+            excludeSender(address(this));
+            excludeSender(address(asset));
+            excludeSender(address(lenderImplementation));
+            excludeSender(address(rateModel));
+            excludeSender(address(vault));
+            excludeSender(address(vaultHarness));
+        }
+
+        thingsThatShouldntChange = ThingsThatShouldntChange(
+            vault.name(),
+            vault.symbol(),
+            vault.decimals(),
+            vault.asset(),
+            vault.DOMAIN_SEPARATOR()
+        );
+    }
+
+    function invariant_thingsThatShouldntChangeDontChange() public {
+        ThingsThatShouldntChange memory update = ThingsThatShouldntChange(
+            vault.name(),
+            vault.symbol(),
+            vault.decimals(),
+            vault.asset(),
+            vault.DOMAIN_SEPARATOR()
+        );
+
+        assertEq(bytes(update.name), bytes(thingsThatShouldntChange.name));
+        assertEq(bytes(update.symbol), bytes(thingsThatShouldntChange.symbol));
+        assertEq(update.decimals, thingsThatShouldntChange.decimals);
+        assertEq(address(update.asset), address(thingsThatShouldntChange.asset));
+        assertEq(update.domainSeparator, thingsThatShouldntChange.domainSeparator);
+    }
+
+    function invariant_conversionsAreInverses() public {
+        // Choice of `x` is random/convenient
+        uint256 x = vault.totalSupply();
+
+        // This would need to be true for all `x` in order to fully prove they're inverses
+        assertEq(vault.convertToAssets(vault.convertToShares(x)), x);
+        assertEq(vault.convertToShares(vault.convertToAssets(x)), x);
+    }
+
+    function invariant_totalSupplyEqualsSumOfBalances() public {
+        uint256 totalSupply;
+        uint256 count = vaultHarness.getHolderCount();
+        for (uint256 i = 0; i < count; i++) {
+            totalSupply += vault.balanceOf(vaultHarness.holders(i));
+        }
+        assertEq(totalSupply, vault.totalSupply());
+    }
+
+    function invariant_maxRedeemLessThanBalance() public {
+        uint256 count = vaultHarness.getHolderCount();
+        for (uint256 i = 0; i < count; i++) {
+            address user = vaultHarness.holders(i);
+
+            assertLe(vault.maxRedeem(user), vault.balanceOf(user));
+        }
+    }
+}
