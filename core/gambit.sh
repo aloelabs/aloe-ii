ae10801d30822114679eed6e2d2002a4e8646158
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2023-07-02 00:45:09 -0500

    Set up gambit mutation testing (#108)

---
+++ core/gambit.sh
@@ -0,0 +1,17 @@
+gambit mutate --json gambit.json
+
+for OUTPUT in $(ls gambit_out/mutants)
+do
+    cp -r "gambit_out/mutants/$OUTPUT/" .
+
+    (FOUNDRY_PROFILE='gambit' forge test -vv --match-contract "LenderInvariantsTest")
+    passed=$?
+
+    git restore test/invariants/LenderHarness.sol
+
+    if [ "${passed}" == "0" ]; then
+        gambit summary
+        echo "❌ Invariant tests passed when they shouldn't have (mutation $OUTPUT)"
+        exit 1
+    fi
+done
