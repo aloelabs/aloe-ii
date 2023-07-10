94b33acdebe0aefe088ee663d61b100db7821eeb
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2023-07-10 17:11:12 -0500

    Address slither's dead code complaints

--- periphery/src/libraries/Uniswap.sol
+++ periphery/src/libraries/Uniswap.sol
@@ -2,13 +2,16 @@
 pragma solidity 0.8.17;
 
 import {Math} from "openzeppelin-contracts/utils/math/Math.sol";
+import {SafeCastLib} from "solmate/utils/SafeCastLib.sol";
 import {IUniswapV3Pool} from "v3-core/contracts/interfaces/IUniswapV3Pool.sol";
 
-import {Q128} from "aloe-ii-core/libraries/constants/Q.sol";
+import {Q96, Q128} from "aloe-ii-core/libraries/constants/Q.sol";
 import {LiquidityAmounts} from "aloe-ii-core/libraries/LiquidityAmounts.sol";
 import {TickMath} from "aloe-ii-core/libraries/TickMath.sol";
 
 library Uniswap {
+    using SafeCastLib for uint256;
+
     struct Position {
         // the lower tick of a position
         int24 lower;
@@ -94,27 +97,55 @@ library Uniswap {
     // ⬆️⬆️⬆️⬆️ VIEW FUNCTIONS ⬆️⬆️⬆️⬆️  ------------------------------------------------------------------------------
     // ⬇️⬇️⬇️⬇️ PURE FUNCTIONS ⬇️⬇️⬇️⬇️  ------------------------------------------------------------------------------
 
-    /// @dev Wrapper around `LiquidityAmounts.getLiquidityForAmount0()`.
     function liquidityForAmount0(Position memory position, uint256 amount0) internal pure returns (uint128) {
         return
-            LiquidityAmounts.getLiquidityForAmount0(
+            liquidityForAmount0(
                 TickMath.getSqrtRatioAtTick(position.lower),
                 TickMath.getSqrtRatioAtTick(position.upper),
                 amount0
             );
     }
 
-    /// @dev Wrapper around `LiquidityAmounts.getLiquidityForAmount1()`.
+    /// @notice Computes the amount of liquidity received for a given amount of token0 and price range
+    /// @dev Calculates amount0 * (sqrt(upper) * sqrt(lower)) / (sqrt(upper) - sqrt(lower))
+    /// @param sqrtRatioAX96 A sqrt price representing the first tick boundary
+    /// @param sqrtRatioBX96 A sqrt price representing the second tick boundary
+    /// @param amount0 The amount0 being sent in
+    /// @return liquidity The amount of returned liquidity
+    function liquidityForAmount0(
+        uint160 sqrtRatioAX96,
+        uint160 sqrtRatioBX96,
+        uint256 amount0
+    ) internal pure returns (uint128 liquidity) {
+        assert(sqrtRatioAX96 < sqrtRatioBX96);
+        uint256 intermediate = Math.mulDiv(sqrtRatioAX96, sqrtRatioBX96, Q96);
+        liquidity = Math.mulDiv(amount0, intermediate, sqrtRatioBX96 - sqrtRatioAX96).safeCastTo128();
+    }
+
     function liquidityForAmount1(Position memory position, uint256 amount1) internal pure returns (uint128) {
         return
-            LiquidityAmounts.getLiquidityForAmount1(
+            liquidityForAmount1(
                 TickMath.getSqrtRatioAtTick(position.lower),
                 TickMath.getSqrtRatioAtTick(position.upper),
                 amount1
             );
     }
 
-    /// @dev Wrapper around `LiquidityAmounts.getLiquidityForAmounts()`.
+    /// @notice Computes the amount of liquidity received for a given amount of token1 and price range
+    /// @dev Calculates amount1 / (sqrt(upper) - sqrt(lower)).
+    /// @param sqrtRatioAX96 A sqrt price representing the first tick boundary
+    /// @param sqrtRatioBX96 A sqrt price representing the second tick boundary
+    /// @param amount1 The amount1 being sent in
+    /// @return liquidity The amount of returned liquidity
+    function liquidityForAmount1(
+        uint160 sqrtRatioAX96,
+        uint160 sqrtRatioBX96,
+        uint256 amount1
+    ) internal pure returns (uint128 liquidity) {
+        assert(sqrtRatioAX96 < sqrtRatioBX96);
+        liquidity = Math.mulDiv(amount1, Q96, sqrtRatioBX96 - sqrtRatioAX96).safeCastTo128();
+    }
+
     function liquidityForAmounts(
         Position memory position,
         uint160 sqrtPriceX96,
@@ -122,13 +153,42 @@ library Uniswap {
         uint256 amount1
     ) internal pure returns (uint128) {
         return
-            LiquidityAmounts.getLiquidityForAmounts(
+            liquidityForAmounts(
                 sqrtPriceX96,
                 TickMath.getSqrtRatioAtTick(position.lower),
                 TickMath.getSqrtRatioAtTick(position.upper),
                 amount0,
                 amount1
             );
+    }
+
+    /// @notice Computes the maximum amount of liquidity received for a given amount of token0, token1, the current
+    /// pool prices and the prices at the tick boundaries
+    /// @param sqrtRatioX96 A sqrt price representing the current pool prices
+    /// @param sqrtRatioAX96 A sqrt price representing the first tick boundary
+    /// @param sqrtRatioBX96 A sqrt price representing the second tick boundary
+    /// @param amount0 The amount of token0 being sent in
+    /// @param amount1 The amount of token1 being sent in
+    /// @return liquidity The maximum amount of liquidity received
+    function liquidityForAmounts(
+        uint160 sqrtRatioX96,
+        uint160 sqrtRatioAX96,
+        uint160 sqrtRatioBX96,
+        uint256 amount0,
+        uint256 amount1
+    ) internal pure returns (uint128 liquidity) {
+        assert(sqrtRatioAX96 < sqrtRatioBX96);
+
+        if (sqrtRatioX96 <= sqrtRatioAX96) {
+            liquidity = liquidityForAmount0(sqrtRatioAX96, sqrtRatioBX96, amount0);
+        } else if (sqrtRatioX96 < sqrtRatioBX96) {
+            uint128 liquidity0 = liquidityForAmount0(sqrtRatioX96, sqrtRatioBX96, amount0);
+            uint128 liquidity1 = liquidityForAmount1(sqrtRatioAX96, sqrtRatioX96, amount1);
+
+            liquidity = liquidity0 < liquidity1 ? liquidity0 : liquidity1;
+        } else {
+            liquidity = liquidityForAmount1(sqrtRatioAX96, sqrtRatioBX96, amount1);
+        }
     }
 
     /// @dev Wrapper around `LiquidityAmounts.getAmountsForLiquidity()`.
