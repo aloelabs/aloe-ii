80e16e718273eec635ea8fa60afedd6b4e06d26c
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2023-08-15 20:55:47 -0500

    meta 3

--- docs.sh
+++ docs.sh
@@ -2,14 +2,32 @@
 
 cd core
 forge doc
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS (Darwin) sed command
+    find docs -type f -name '*.md' -print0 | xargs -0 sed -i.bak -E 's|github.com\/aloelabs\/aloe-ii\/blob\/([0-9a-fA-F]+)\/|github.com/aloelabs/aloe-ii/blob/\1/core/|g'
+    find docs -type f -name '*.md' -print0 | xargs -0 sed -i.bak -E 's|\/src\/(.*)\.md|/core/\1.md|g'
+else
+    # Linux (including Ubuntu) sed command
+    sed -i -E 's|github.com\/aloelabs\/aloe-ii\/blob\/([0-9a-fA-F]+)\/|github.com/aloelabs/aloe-ii/blob/\1/core/|g'
+    sed -i -E 's|\/src\/(.*)\.md|/core/\1.md|g'
+fi
 cd ..
 
-rm -r docs/src/core
-mv docs/src/src docs/src/core
+cp -rf core/docs/src/src/ docs/src/core
+rm -rf core/docs
 
 cd periphery
 forge doc
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS (Darwin) sed command
+    find docs -type f -name '*.md' -print0 | xargs -0 sed -i.bak -E 's|github.com\/aloelabs\/aloe-ii\/blob\/([0-9a-fA-F]+)\/|github.com/aloelabs/aloe-ii/blob/\1/periphery/|g'
+    find docs -type f -name '*.md' -print0 | xargs -0 sed -i.bak -E 's|\/src\/(.*)\.md|/periphery/\1.md|g'
+else
+    # Linux (including Ubuntu) sed command
+    sed -i -E 's|github.com\/aloelabs\/aloe-ii\/blob\/([0-9a-fA-F]+)\/|github.com/aloelabs/aloe-ii/blob/\1/periphery/|g'
+    sed -i -E 's|\/src\/(.*)\.md|/periphery/\1.md|g'
+fi
 cd ..
 
-rm -r docs/src/periphery
-mv docs/src/src docs/src/periphery
+cp -rf periphery/docs/src/src/ docs/src/periphery
+rm -rf periphery/docs
