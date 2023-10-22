4caf681fca892aa2314959a5ed45ced76b3442a6
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2023-10-22 14:18:10 -0500

    Move SafeSSTORE2 into ERC721Z file

--- periphery/src/borrower-nft/ERC721Z.sol
+++ periphery/src/borrower-nft/ERC721Z.sol
@@ -4,7 +4,16 @@ pragma solidity 0.8.17;
 import {SSTORE2} from "solady/utils/SSTORE2.sol";
 
 import {BytesLib} from "./BytesLib.sol";
-import {SafeSSTORE2} from "./SafeSSTORE2.sol";
+
+library SafeSSTORE2 {
+    function write(bytes memory data) internal returns (address pointer) {
+        pointer = (data.length == 0) ? address(0) : SSTORE2.write(data);
+    }
+
+    function read(address pointer) internal view returns (bytes memory data) {
+        data = (pointer == address(0)) ? bytes("") : SSTORE2.read(pointer);
+    }
+}
 
 /**
  * @title ERC721Z
