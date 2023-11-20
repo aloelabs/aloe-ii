8bc10d1765770cc766f7f524d8bcdfc88c9a783d
ianwoodard <17186604+IanWoodard@users.noreply.github.com>
2023-11-19 20:39:28 -0800

    Adding an even simpler manager

---
+++ periphery/src/managers/BorrowerNFTSimpleManager.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.17;
+
+import {IManager} from "aloe-ii-core/Borrower.sol";
+
+contract BorrowerNFTSimpleManager is IManager {
+    function callback(bytes calldata data, address, uint208) external override returns (uint208) {
+        (bool success, ) = msg.sender.call(data[20:]); // solhint-disable-line avoid-low-level-calls
+        require(success);
+        return 0;
+    }
+}
